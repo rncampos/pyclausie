2e938507a0bfda711e41ce8336473308aa21b054
rncampos <ricardo.campos@ipt.pt>
2021-07-08 02:10:30 +0100

    fix codec issues

--- test.py
+++ test.py
@@ -1,11 +1,9 @@
 from pyclausie import ClausIE
 cl = ClausIE.get_instance()
 
-sents = ["Política Canção Britain's."]
-#sents = ["After gaël giving 5,000 people a second chance at life, doctors are celebrating the 25th anniversary of Britain's first heart transplant."]
-#sents = ["He wan After her"]
-#sents = ["Marie was born in Paris. She was born's in France."]
-#sents = ['António Pires de Lima, que mantém divergências políticas públicas com Nobre Guedes, considerou que a ausência do ex-ministro do Ambiente vai limitar o âmbito do próprio Congresso.']
+
+sents = ["Marie was born in Paris. She was born in France."]
+
 triples = cl.extract_triples(sents)
 if triples != None:
     for triple in triples:
